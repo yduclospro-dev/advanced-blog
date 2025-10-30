79e00989e3cba22e526d2fc1504043012d6b7b72
yduclos <yduclos@sqli.com>
2025-10-30 10:03:48 +0100

    feat: :card_file_box: Add role column into User entity

--- Domain/entities/User.ts
+++ Domain/entities/User.ts
@@ -1,14 +1,18 @@
+import { UserRole } from "@prisma/client";
+
 export class User {
   public id: string;
   public userName: string;
   public email: string;
   public password: string;
+  public role: UserRole;
 
   constructor(id: string, userName: string, email: string, password: string) {
     this.id = id;
     this.userName = userName;
     this.email = email;
     this.password = password;
+    this.role = UserRole.USER;
   }
 
   setPassword(newPassword: string) {
