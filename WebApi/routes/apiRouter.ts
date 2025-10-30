8ae18e7e94f09d3591b064c8fb3f09fff4163dde
Jeremy C <chanft.jeremy@gmail.com>
2025-10-30 10:16:57 +0100

    feat: add password reset routes

--- WebApi/routes/apiRouter.ts
+++ WebApi/routes/apiRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController.ts";
+import { PasswordController } from "../controllers/PasswordController.ts";
 
 const apiRouter = Router();
 
 const userController = new UserController();
+const passwordController = new PasswordController();
 
 apiRouter.get('/status', (req, res) => {
   res.json({ status: 'ok' })
@@ -11,5 +13,9 @@ apiRouter.get('/status', (req, res) => {
 
 apiRouter.post('/register', userController.register.bind(userController));
 apiRouter.post('/login', userController.login.bind(userController));
+
+// Password reset routes
+apiRouter.post('/password/forgot', passwordController.forgotPassword.bind(passwordController));
+apiRouter.post('/password/reset', passwordController.resetPassword.bind(passwordController));
 
 export default apiRouter;
