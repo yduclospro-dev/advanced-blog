69ca1d7e0794fc36c49a937467484e86f8211718
NEVE <loris095@gmail.com>
2025-10-30 16:34:05 +0100

    feat: :sparkles: Correction des migrations

--- prisma/migrations/20251030152904_add_articles/migration.sql
+++ prisma/migrations/20251030152904_add_articles/migration.sql
@@ -1,14 +1,4 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "userName" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Article" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
@@ -18,12 +8,6 @@ CREATE TABLE "Article" (
 
     CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
