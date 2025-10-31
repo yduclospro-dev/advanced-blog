dbc2adcae5445ad03c6377f5c991b27c2ebe48a8
yduclos <yduclos@sqli.com>
2025-10-31 08:44:18 +0100

    refactor(articles): use userName for article and comment authors
    - Update article creation to use currentUser.userName
    - Update comment creation to use currentUser.userName

--- src/components/articles/containers/ArticleDetailContainer.tsx
+++ src/components/articles/containers/ArticleDetailContainer.tsx
@@ -69,7 +69,7 @@ export default function ArticleDetailContainer() {
         addComment({
             articleId: article.id,
             authorId: currentUser.id,
-            authorName: currentUser.username,
+            authorName: currentUser.userName,
             content,
         });
         setToast({ message: "Commentaire ajouté avec succès !", type: "success" });
