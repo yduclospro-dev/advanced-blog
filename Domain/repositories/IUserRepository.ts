dffeed4b46925b0f433d372500ee95aedaa4e714
yduclos <yduclos@sqli.com>
2025-10-31 08:44:01 +0100

    feat(domain): add findById method to user repository

--- Domain/repositories/IUserRepository.ts
+++ Domain/repositories/IUserRepository.ts
@@ -3,4 +3,5 @@ import { User } from "../entities/User.ts";
 export interface IUserRepository {
   create(user: User): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
 }
