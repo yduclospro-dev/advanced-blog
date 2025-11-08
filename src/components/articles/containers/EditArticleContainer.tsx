50fbfd14feceee2525b810ba8886046e9401220a
yduclos <yduclos@sqli.com>
2025-11-08 21:24:19 +0100

    fix(articles): redirect to articles list after edit instead of detail page

--- src/components/articles/containers/EditArticleContainer.tsx
+++ src/components/articles/containers/EditArticleContainer.tsx
@@ -47,7 +47,7 @@ export default function EditArticleContainer() {
         setToast({ message: "Article modifié avec succès !", type: "success" });
         
         setTimeout(() => {
-          router.push(`/articles/${article.id}`);
+          router.push('/articles');
         }, 1500);
       } catch (error) {
         setToast({ 
