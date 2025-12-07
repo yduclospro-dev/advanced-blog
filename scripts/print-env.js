add27df57262221106b970915bdf6f53ea78a9f2
yduclos <yduclos@sqli.com>
2025-12-07 15:04:30 +0100

    Remove unused print-env.js script

--- scripts/print-env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-console.log("DOTENV_CONFIG_PATH =", process.env.DOTENV_CONFIG_PATH);
-
-dotenv.config({
-  path: process.env.DOTENV_CONFIG_PATH || ".env"
-});
-
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
